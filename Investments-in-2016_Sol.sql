5ce822765e93cd4e23bc98d894dd7fd1711fceff
Sara Whdan <109487290+saraWhdan@users.noreply.github.com>
2023-09-25 18:21:05 +0300

    Create Investments-in-2016_Sol.sql

---
+++ Investments-in-2016_Sol.sql
@@ -0,0 +1,20 @@
+/* Write your T-SQL query statement below */
+
+
+
+WITH CTE AS (
+		SELECT
+			*,
+			COUNT(lat) OVER(PARTITION BY lat,lon) CountLatLon,
+			COUNT(tiv_2015) OVER(PARTITION BY tiv_2015) CountT_2015
+		FROM
+			Insurance T1
+)
+SELECT
+	ROUND(SUM(tiv_2016),2) tiv_2016 
+FROM
+	CTE
+WHERE
+	CountLatLon = 1
+AND
+	CountT_2015 > 1
